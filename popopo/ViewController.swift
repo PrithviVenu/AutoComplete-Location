8d21111b35acfb24676c20c50c394152e97322e3
Prithvi Venu <prithviv30@gmail.com>
2019-06-09 00:02:27 +0530

    Initial Commit

---
+++ popopo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  popopo
+//
+//  Created by prithvi-pt2335 on 09/06/19.
+//  Copyright © 2019 prithvi-pt2335. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
